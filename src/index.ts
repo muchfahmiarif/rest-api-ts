9eaec361a45b9bf87274c9c16b4d6beb5c6b714f
muchfahmiarif <fahmiarif96@gmail.com>
2023-06-02 11:19:39 +0700

    `init` install express, ts, nodemon; confog nodemon and create simple api

---
+++ src/index.ts
@@ -0,0 +1,13 @@
+import express, { Application, NextFunction, Request, Response } from "express";
+
+const app: Application = express();
+const port: Number = 4000;
+
+app.listen(port, () => console.log(`Server running on port ${port}`));
+
+app.use("/", (req: Request, res: Response, next: NextFunction) => {
+  res.status(200).json({
+    data: "Hello World",
+  });
+});
+// console.log("Hello World");
