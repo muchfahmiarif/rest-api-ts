c42871df7f40888f81a90a94081dc85a46036184
muchfahmiarif <fahmiarif96@gmail.com>
2023-07-18 02:46:24 +0700

    `init` setup prettier and eslint

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,12 @@
-import express, { Application, NextFunction, Request, Response } from "express";
+import express, { Application, NextFunction, Request, Response } from 'express';
 
 const app: Application = express();
 const port: Number = 4000;
 
 app.listen(port, () => console.log(`Server running on port ${port}`));
 
-app.use("/", (req: Request, res: Response, next: NextFunction) => {
+app.use('/health', (req: Request, res: Response, next: NextFunction) => {
   res.status(200).json({
-    data: "Hello World",
+    data: 'Hello World',
   });
 });
-// console.log("Hello World");
