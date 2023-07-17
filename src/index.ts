dbd270165d244bc1dbc8d29a63dcb8bfabfda8dd
muchfahmiarif <fahmiarif96@gmail.com>
2023-07-18 03:17:32 +0700

    linting adn formatting

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,14 @@
-import express, { Application, NextFunction, Request, Response } from 'express';
+import express, { type Application, type NextFunction, type Request, type Response } from 'express'
 
-const app: Application = express();
-const port: Number = 4000;
+const app: Application = express()
+const port: number = 4000
 
-app.listen(port, () => console.log(`Server running on port ${port}`));
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`)
+})
 
 app.use('/health', (req: Request, res: Response, next: NextFunction) => {
   res.status(200).json({
-    data: 'Hello World',
-  });
-});
+    data: 'Hello World'
+  })
+})
