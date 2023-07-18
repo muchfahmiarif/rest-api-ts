2ebb83472bf97d2f6fb294cf5de0ff706842d103
Muchammad Fahmi Arif <fahmiarif96@gmail.com>
2023-07-18 11:57:09 +0700

    `validation` implement validation with joi library

--- src/routes/product.route.ts
+++ src/routes/product.route.ts
@@ -1,5 +1,6 @@
 import { Router, type Request, type Response, type NextFunction } from 'express'
 import { logger } from '../utils/logger'
+import { createProductValidation } from '../validations/product.validation'
 
 export const ProductRouter: Router = Router()
 
@@ -25,11 +26,21 @@ ProductRouter.get('/', (req: Request, res: Response, next: NextFunction) => {
 
 // post untuk menambah data
 ProductRouter.post('/', (req: Request, res: Response, next: NextFunction) => {
+  const { error, value } = createProductValidation(req.body)
+  if (error != null) {
+    logger.error('Error add product data', error.details[0].message)
+    res.status(422).send({
+      status: false,
+      statusCode: 422,
+      message: error.details[0].message,
+      data: null
+    })
+  }
   logger.info('Success add product data')
   res.status(200).send({
     status: true,
     statusCode: 200,
     message: 'Success',
-    data: req.body
+    data: value // value = req.body, tetapi value hasil validasi dari createProductValidation
   })
 })
