2805359a8aa6ac84dc7f7ccf4a5e9f19a01c5f61
muchfahmiarif <fahmiarif96@gmail.com>
2023-07-22 06:34:03 +0700

    `database` add service to create data and add controller

--- src/controllers/product.controller.ts
+++ src/controllers/product.controller.ts
@@ -1,29 +1,41 @@
 import { type Request, type Response, type NextFunction } from 'express'
 import { logger } from '../utils/logger'
 import { createProductValidation } from '../validations/product.validation'
-import { getProductFromDB } from '../services/product.service'
+import { createProductToDB, getProductFromDB } from '../services/product.service'
 import { v4 as uuidv4 } from 'uuid'
 import { type ProductType } from '../types/product.type'
 
-export const createProduct = (req: Request, res: Response, next: NextFunction) => {
+export const createProduct = async (req: Request, res: Response, next: NextFunction) => {
   req.body.product_id = uuidv4()
   const { error, value } = createProductValidation(req.body)
   if (error != null) {
     logger.error('Error add product data', error.details[0].message)
-    res.status(422).send({
+    return res.status(422).send({
       status: false,
       statusCode: 422,
       message: error.details[0].message,
       data: null
     })
   }
-  logger.info('Success add product data')
-  res.status(200).send({
-    status: true,
-    statusCode: 200,
-    message: 'Success',
-    data: value // value = req.body, tetapi value hasil validasi dari createProductValidation
-  })
+
+  try {
+    await createProductToDB(value)
+    logger.info('Success add product data')
+    return res.status(201).send({
+      status: true,
+      statusCode: 201,
+      message: 'Add Data Success',
+      data: value // value = req.body, tetapi value hasil validasi dari createProductValidation
+    })
+  } catch (error) {
+    logger.error('Error add product data', error)
+    return res.status(500).send({
+      status: false,
+      statusCode: 500,
+      message: 'Internal Server Error',
+      data: null
+    })
+  }
 }
 
 export const getProduct = async (req: Request, res: Response, next: NextFunction) => {
--- src/services/product.service.ts
+++ src/services/product.service.ts
@@ -1,5 +1,6 @@
 import { logger } from '../utils/logger'
 import productModel from '../models/product.model'
+import { type ProductType } from '../types/product.type'
 
 export const getProductFromDB = async () => {
   return await productModel
@@ -11,4 +12,8 @@ export const getProductFromDB = async () => {
       logger.info('Error get product data', error)
       logger.error(error)
     })
+}
+
+export const createProductToDB = async (payload: ProductType) => {
+  return await productModel.create(payload) // model from mongoose
 }
